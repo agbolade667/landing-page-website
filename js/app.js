fe5f8af273896957dbd231476e28adb9d2e9e998
Agbolade Omowole <agboladeomowole@gmail.com>
2020-05-18 11:05:10 +0100

    style: write JavaScript variables to get section elements

--- js/app.js
+++ js/app.js
@@ -26,6 +26,28 @@
 */
 
 
+let unorderedList = document.getElementById("unordered-list");
+let containerCount = document.getElementsByClassName("landing__container")
+  .length;
+
+for (ilet k = 1; k < containerCount + 1; k++) {
+  let item = "#section" + i + "-title";
+  let item_value = document.querySelector(item);
+  let item_text = item_value.textContent;
+  let new_line = document.createElement("li");
+  let line_text = document.createTextNode(item_text);
+  let list_item = "sample-nav-" + i;
+
+  new_line.setAttribute("id", list_item);
+  new_line.setAttribute("class", "nav-list-item");
+  new_line.appendChild(line_text);
+  unorderedList.appendChild(new_line);
+
+  let itemTarget = document.getElementById("section" + i);
+  let listTarget = document.getElementById(listItem);
+  let buttonName = "section" + i + "-button";
+  let buttonToAdd = document.getElementById(buttonName);
+
 
 /**
  * End Helper Functions
