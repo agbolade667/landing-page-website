c693e573f4e4c61d6ed82d9255a85623ebedaf28
Agbolade Omowole <agboladeomowole@gmail.com>
2020-05-18 12:45:30 +0100

    feat: added an event listener for Click

--- js/app.js
+++ js/app.js
@@ -30,33 +30,48 @@ let unorderedList = document.getElementById("unordered-list");
 let containerCount = document.getElementsByClassName("landing__container")
   .length;
 
-for (ilet k = 1; k < containerCount + 1; k++) {
-  let item = "#section" + i + "-title";
+for (let k = 1; k < (containerCount + 1); k++) {
+  let item = "#section" + k + "-title";
   let item_value = document.querySelector(item);
   let item_text = item_value.textContent;
   let new_line = document.createElement("li");
   let line_text = document.createTextNode(item_text);
-  let list_item = "sample-nav-" + i;
+  let list_item = "sample-nav-" + k;
 
   new_line.setAttribute("id", list_item);
   new_line.setAttribute("class", "nav-list-item");
   new_line.appendChild(line_text);
   unorderedList.appendChild(new_line);
 
-  let itemTarget = document.getElementById("section" + i);
-  let listTarget = document.getElementById(listItem);
-  let buttonName = "section" + i + "-button";
-  let buttonToAdd = document.getElementById(buttonName);
+  let item_target = document.getElementById("section" + k);
+  let list_target = document.getElementById(list_item);
+  let button_name = "section" + k + "-button";
+  let button_to_add = document.getElementById(buttonName);
 
 
 /**
+ */
+ 
+list_target.addEventListener("click", function() {
+    item_target.scrollIntoView ({
+      behavior: 'smooth'}
+    )
+//Added behavior smooth as per review. There is now a visible scroll
+    button_to_add.innerHTML =
+      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
+  });
+}
+
+ /* 
  * End Helper Functions
+ * 
+ * 
+ * 
  * Begin Main Functions
  * 
 */
 
 // build the nav
-
 
 // Add class 'active' to section when near top of viewport
 
